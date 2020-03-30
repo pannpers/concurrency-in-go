94e4ebb2b4c24f415e2995d5348784a58bae8550
pannpers <pepperoni9@gmail.com>
2020-03-30 11:59:13 +0900

    add chapter 3

---
+++ chapter-3/memory-consumption/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+
+	const numGoroutine = 1e4
+	wg.Add(numGoroutine)
+	before := memConsumed()
+
+	for i := numGoroutine; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutine/1000)
+}
